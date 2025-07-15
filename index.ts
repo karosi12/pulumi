9f877320fd65b2ea8da48ce7c90b86975da60af8
karosi12 <adekayor@gmail.com>
2025-07-15 20:00:46 +0100

    fix: remove subnet id

--- index.ts
+++ index.ts
@@ -3,6 +3,5 @@ import * as compute from "./src/stacks/compute";
 import * as storage from "./src/stacks/storage";
 
 export const vpcId = network.vpcId;
-export const subnetId = network.subnetId;
 export const instanceId = compute.instance.id;
 export const bucketId = storage.bucketName;
